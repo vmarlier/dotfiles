dc5af3cc4a20e49e55c30845ba496332afce0c41
Valentin Marlier <vmarlier@talend.com>
2021-08-23 18:56:19 +0200

    feat: add keymaps, basic options

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,32 +1,95 @@
--- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
-
 -- general
 lvim.format_on_save = true
 lvim.lint_on_save = true
 lvim.colorscheme = "spacegray"
 
+-- vim options
+vim.opt.backup = false -- creates a backup file
+vim.opt.clipboard = "" -- allows neovim to access the system clipboard
+vim.opt.cmdheight = 2 -- more space in the neovim command line for displaying messages
+vim.opt.colorcolumn = "99999" -- fixes indentline for now
+vim.opt.completeopt = { "menuone", "noselect" }
+vim.opt.conceallevel = 0 -- so that `` is visible in markdown files
+vim.opt.fileencoding = "utf-8" -- the encoding written to a file
+vim.opt.foldmethod = "manual" -- folding, set to "expr" for treesitter based folding
+vim.opt.foldexpr = "" -- set to "nvim_treesitter#foldexpr()" for treesitter based folding
+vim.opt.hidden = true -- required to keep multiple buffers and open multiple buffers
+vim.opt.hlsearch = true -- highlight all matches on previous search pattern
+vim.opt.ignorecase = true -- ignore case in search patterns
+vim.opt.mouse = "a" -- allow the mouse to be used in neovim
+vim.opt.pumheight = 10 -- pop up menu height
+vim.opt.showmode = true -- we don't need to see things like -- INSERT -- anymore
+vim.opt.showtabline = 2 -- always show tabs
+vim.opt.smartcase = true -- smart case
+vim.opt.smartindent = true -- make indenting smarter again
+vim.opt.splitbelow = true -- force all horizontal splits to go below current window
+vim.opt.splitright = true -- force all vertical splits to go to the right of current window
+vim.opt.swapfile = false -- creates a swapfile
+vim.opt.termguicolors = true -- set term gui colors (most terminals support this)
+vim.opt.timeoutlen = 100 -- time to wait for a mapped sequence to complete (in milliseconds)
+vim.opt.title = true -- set the title of window to the value of the titlestring
+vim.opt.undodir = CACHE_PATH .. "/undo" -- set an undo directory
+vim.opt.undofile = true -- enable persistent undo
+vim.opt.updatetime = 300 -- faster completion
+vim.opt.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+vim.opt.expandtab = true -- convert tabs to spaces
+vim.opt.shiftwidth = 2 -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 2 -- insert 2 spaces for a tab
+vim.opt.cursorline = true -- highlight the current line
+vim.opt.number = true -- set numbered lines
+vim.opt.relativenumber = false -- set relative numbered lines
+vim.opt.numberwidth = 4 -- set number column width to 2 {default 4:}
+vim.opt.signcolumn = "yes" -- always show the sign column, otherwise it would shift the text each time
+vim.opt.wrap = false -- display lines as one long line
+vim.opt.spell = false
+vim.opt.spelllang = "en"
+vim.opt.scrolloff = 8 -- is one of my fav
+vim.opt.sidescrolloff = 8
+
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
--- add your own keymapping
-lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
--- unmap a default keymapping
--- lvim.keys.normal_mode["<C-Up>"] = ""
--- edit a default keymapping
--- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
+lvim.keys.normal_mode = {
+  -- Buffers and Tabs
+  ["<S-n>"] = ":bnext<CR>",
+  ["<S-b>"] = ":bprevious<CR>",
+  ["<S-d>"] = ":bdelete<CR>",
+  ["<S-t>"] = ":tabnew<CR>",
+  -- Terminal
+  ["<C-t>"] = "<cmd>ToggleTerm<CR>",
+  -- View
+  ["<C-v>"] = ":vsplit<CR>",
+  ["<C-s>"] = ":split<CR>",
+  -- Move around
+  ["<C-j>"] = "<C-W>j",
+  ["<C-k>"] = "<C-W>k",
+  ["<C-h>"] = "<C-W>h",
+  ["<C-l>"] = "<C-W>l",
+  -- Only use hjkl
+  ["<left>"] = "<nop>",
+  ["<right>"] = "<nop>",
+  ["<up>"] = "<nop>",
+  ["<down>"] = "<nop>",
+  -- (Un)Comment
+  ["<space>/"] = "",
+  ["<S-c>"] = "<cmd>CommentToggle<CR>",
+  -- Telescope
+  ["<Tab>t"] = "<cmd>Telescope<CR>",
+  ["<Tab>gc"] = "<cmd>Telescope git_commits<CR>",
+  ["<Tab>gb"] = "<cmd>Telescope git_branches<CR>",
+  ["<Tab>gs"] = "<cmd>Telescope git_status<CR>",
+  ["<Tab>f"] = "<cmd>Telescope find_files<CR>",
+  ["<Tab>o"] = "<cmd>Telescope oldfiles<CR>",
+}
 
--- Use which-key to add extra bindings with the leader-key prefix
--- lvim.builtin.which_key.mappings["P"] = { "<cmd>lua require'telescope'.extensions.project.project{}<CR>", "Projects" }
--- lvim.builtin.which_key.mappings["t"] = {
---   name = "+Trouble",
---   r = { "<cmd>Trouble lsp_references<cr>", "References" },
---   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
---   d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnosticss" },
---   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
---   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
---   w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnosticss" },
+-- Autocommands
+-- lvim.autocommands.custom_groups = {
+   -- On entering a lua file, set the tab spacing and shift width to 8
+   -- { "BufWinEnter", "*.lua", "setlocal ts=8 sw=8" },
+
+  -- On entering insert mode in any file, scroll the window so the cursor line is centered
+  -- {"InsertEnter", "*", ":normal zz"},
 -- }
 
--- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
