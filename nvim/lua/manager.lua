c5a511e1caba7c69f6c1d5ff99cb13b89d1f321d
Valentin Marlier <vmarlier@talend.com>
2021-10-30 12:22:39 +0200

    feat: use packer instead of PAQ

--- nvim/lua/manager.lua
+++ nvim/lua/manager.lua
@@ -3,59 +3,72 @@
 -----------------------------------
 -- Sections:
 -- -> Bootstrapping
+-- -> Auto-Sync
 -- -> Modules
 
 -------------------
 -- Bootstrapping --
 -------------------
 local fn = vim.fn
-
-local install_path = fn.stdpath('data') .. '/site/pack/paqs/start/paq-nvim'
-
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', '--depth=1', 'https://github.com/savq/paq-nvim.git', install_path})
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
+
+---------------
+-- Auto-Sync --
+---------------
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost manager.lua source <afile> | PackerCompile
+  augroup end
+]])
 
 -------------
 -- Modules --
 -------------
-require "paq" {
-    "savq/paq-nvim"; -- let Paq manage itself
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
 
-    -- Completion
-    "neovim/nvim-lspconfig"; -- a collection of common configurations for Neovim's built-in language server client.
-    "hrsh7th/nvim-cmp"; -- completion tool
+  -- Coc and Ale
 
-    -- Dashboard, Helper, Term..
-    "glepnir/dashboard-nvim"; -- display a dashboard
-    "nvim-lua/plenary.nvim"; -- required for telescope
-    "nvim-telescope/telescope.nvim"; -- highly extandable fuzzy finder
-    "nvim-telescope/telescope-project.nvim"; -- manage project within Telescope
-    "nvim-treesitter/nvim-treesitter"; -- simple and easy way to use the interface for tree-sitter
-    "folke/which-key.nvim"; -- displays a popup with possible keybindings
-    "akinsho/toggleterm.nvim"; -- terminal plugin
+  -- Dashboard, Helper, Term..
+  use 'glepnir/dashboard-nvim' -- display a dashboard
+  use {'nvim-telescope/telescope.nvim', requires='nvim-lua/plenary.nvim'} -- highly extandable fuzzy finder
+  use 'nvim-telescope/telescope-project.nvim' -- manage project within Telescope
+  use {'nvim-treesitter/nvim-treesitter', run=':TSUpdate'} -- simple and easy way to use the interface for tree-sitter
+  use 'folke/which-key.nvim' -- displays a popup with possible keybindings
+  use 'akinsho/toggleterm.nvim' -- terminal plugin
 
-    -- Useful Stuff
-    "lukas-reineke/indent-blankline.nvim"; -- indentation guides to all lines
-    "terrortylor/nvim-comment"; -- comment easily
-    "pseewald/vim-anyfold"; -- fold anything
-    "vim-scripts/align"; -- align stuff:wq
-    "maxbrunsfeld/vim-yankstack"; -- maintains a history of previous yanks, changes and deletes
+  -- Navigation and Visual
+  use {'kyazdani42/nvim-tree.lua', requires='kyazdani42/nvim-web-devicons'} -- nvim tree
+  use {'glepnir/galaxyline.nvim', branch='main'} -- statusline
+  use 'romgrk/barbar.nvim' -- tabline plugin with re-orderable, auto-sizing..
 
-    -- Git
-    "airblade/vim-gitgutter"; -- shows a git diff in the gutter (sign column)
-    "tveskag/nvim-blame-line"; -- git blame in status bar or virtual text
-    "sindrets/diffview.nvim"; -- review all changed files for any git rev.
+  -- Useful Stuff
+  use 'lukas-reineke/indent-blankline.nvim' -- indentation guides to all lines
+  use 'terrortylor/nvim-comment' -- comment easily
+  use 'pseewald/vim-anyfold' -- fold anything
+  use 'vim-scripts/align' -- align stuff:wq
+  use 'maxbrunsfeld/vim-yankstack' -- maintains a history of previous yanks, changes and deletes
 
-    -- Navigation and Visual
-    "kyazdani42/nvim-tree.lua"; -- nvim tree
-    "glepnir/galaxyline.nvim"; -- statusline
-    "romgrk/barbar.nvim"; -- tabline plugin with re-orderable, auto-sizing..
+  -- Git
+  use 'airblade/vim-gitgutter' -- shows a git diff in the gutter (sign column)
+  use 'tveskag/nvim-blame-line' -- git blame in status bar or virtual text
+  use 'sindrets/diffview.nvim' -- review all changed files for any git rev.
 
-    -- Themes
-    "kyazdani42/nvim-web-devicons"; -- lua fork of vim-devicons
-    "joshdick/onedark.vim"; -- colorscheme
+  -- Themes
+  use 'joshdick/onedark.vim' -- colorscheme
 
-    -- Language Specific
-    "iamcco/markdown-preview.nvim"; -- preview markdown files
-}
+  -- Language Specific
+  use {'iamcco/markdown-preview.nvim', ft = {'markdown'} ,run = 'cd app && yarn install', cmd = 'MarkdownPreview'} -- preview mardown files
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
+
