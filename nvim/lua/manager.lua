589bc43bfb69c56b8b0e66226e8cadb937706623
Valentin Marlier <vmarlier@talend.com>
2021-12-24 10:55:50 +0100

    feat(): update colorscheme

--- nvim/lua/manager.lua
+++ nvim/lua/manager.lua
@@ -67,7 +67,7 @@ return require('packer').startup(function(use)
   use 'sindrets/diffview.nvim' -- review all changed files for any git rev.
 
   -- Themes
-  use 'joshdick/onedark.vim' -- colorscheme
+  use 'folke/tokyonight.nvim' -- colorscheme
 
   -- Language Specific
   use {'iamcco/markdown-preview.nvim', ft = {'markdown'}, run = 'cd app && yarn install', cmd = 'MarkdownPreview'} -- preview mardown files
