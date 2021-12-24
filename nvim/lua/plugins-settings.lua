b2f75871c2306a24ceb3590eeac101d82300c3e6
Valentin Marlier <vmarlier@talend.com>
2021-12-24 10:55:05 +0100

    feat(): better blankline configuration, add signs in nvimtree

--- nvim/lua/plugins-settings.lua
+++ nvim/lua/plugins-settings.lua
@@ -197,12 +197,44 @@ require("toggleterm").setup{
 ----------------------
 -- Indent-Blankline --
 ----------------------
-vim.opt.list = true
-vim.opt.listchars:append("space:⋅")
-require("indent_blankline").setup {
-    space_char_blankline = " ",
-    buftype_exclude = {"terminal"},
+vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
+vim.g.indent_blankline_filetype_exclude = {
+  "help",
+  "startify",
+  "dashboard",
+  "packer",
+  "neogitstatus",
+  "NvimTree",
+  "Trouble",
 }
+vim.g.indent_blankline_char = "│"
+vim.g.indent_blankline_use_treesitter = true
+vim.g.indent_blankline_show_trailing_blankline_indent = false
+vim.g.indent_blankline_show_current_context = true
+vim.g.indent_blankline_context_patterns = {
+  "class",
+  "return",
+  "function",
+  "method",
+  "^if",
+  "^while",
+  "jsx_element",
+  "^for",
+  "^object",
+  "^table",
+  "block",
+  "arguments",
+  "if_statement",
+  "else_clause",
+  "jsx_element",
+  "jsx_self_closing_element",
+  "try_statement",
+  "catch_clause",
+  "import_statement",
+  "operation_type",
+}
+-- HACK: work-around for https://github.com/lukas-reineke/indent-blankline.nvim/issues/59
+vim.wo.colorcolumn = "99999"
 
 -----------------
 -- VIM-Anyfold --
@@ -260,7 +292,7 @@ require'nvim-tree'.setup {
   update_cwd          = false,
   -- show lsp diagnostics in the signcolumn
   diagnostics = {
-    enable = false,
+    enable = true,
     icons = {
       hint = "",
       info = "",
@@ -305,6 +337,7 @@ require'nvim-tree'.setup {
     }
   }
 }
+
 
 -----------------
 -- NVIM-Notify --
