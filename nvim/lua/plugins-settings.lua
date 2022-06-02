368439e7ddfa4ad27106f718877d311aea99498b
Valentin Marlier <vmarlier@talend.com>
2022-06-03 00:34:42 +0200

    feat(): nvim-cmp use arrows for navigating through completion items

--- nvim/lua/plugins-settings.lua
+++ nvim/lua/plugins-settings.lua
@@ -57,8 +57,8 @@ cmp.setup {
     end,
   },
   mapping = {
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<UP>'] = cmp.mapping.select_prev_item(),
+    ['<DOWN>'] = cmp.mapping.select_next_item(),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
