b7dd866c8c5a6a6ca34a9227914817d40d2a9922
Valentin Marlier <vmarlier@talend.com>
2022-06-03 16:51:37 +0200

    feat(): update nvim folder readme

--- nvim/lua/manager.lua
+++ nvim/lua/manager.lua
@@ -11,7 +11,7 @@
 -------------------
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
+ if fn.empty(fn.glob(install_path)) > 0 then
   packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 
@@ -46,7 +46,6 @@ return require('packer').startup(function(use)
   use 'glepnir/dashboard-nvim' -- display a dashboard
   use {'nvim-telescope/telescope.nvim', requires='nvim-lua/plenary.nvim'} -- highly extandable fuzzy finder
   use {'nvim-treesitter/nvim-treesitter', run=':TSUpdate'} -- simple and easy way to use the interface for tree-sitter
-  use 'nvim-treesitter/nvim-treesitter-textobjects' -- requirement for goldsmith
   use 'folke/which-key.nvim' -- displays a popup with possible keybindings
   use 'akinsho/toggleterm.nvim' -- terminal plugin
 
