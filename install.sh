be4a53fc0c253928a1f022e5dbecd861a3193bcf
vmarlier <valentin.marlier@pm.me>
2024-03-26 01:22:08 +0100

    feat(): remove sketchybar and related, add yabai and skhd

--- install.sh
+++ install.sh
@@ -15,6 +15,7 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 # brew installations
 brew tap FelixKratz/formulae
+brew tap koekeishiya/formulae
 brew install wget
 brew install git
 brew install ansible
@@ -30,23 +31,13 @@ brew install bat
 brew install fd
 brew install borders
 # sketchybar related
-brew install sketchybar
 brew install lua
 brew install switchaudio-osx
 brew install nowplaying-cli
+brew install koekeishiya/formulae/yabai
+brew install koekeishiya/formulae/skhd
 
 brew services start borders
-brew services start sketchybar
-
-# Fonts
-brew install --cask sf-symbols
-brew install --cask homebrew/cask-fonts/font-sf-mono
-brew install --cask homebrew/cask-fonts/font-sf-pro
-
-curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.5/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
-
-# SbarLua
-(git clone https://github.com/FelixKratz/SbarLua.git /tmp/SbarLua && cd /tmp/SbarLua/ && make install && rm -rf /tmp/SbarLua/)
 
 # pip installations
 pip3 install pew
@@ -95,9 +86,14 @@ git clone https://github.com/vmarlier/nvim.git ~/.config/nvim
 ln -s ~/Git/$(whoami)/dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/Git/$(whoami)/dotfiles/asdf/.tool-versions ~/.tool-versions
 ln -s ~/Git/$(whoami)/dotfiles/borders/bodersrc ~/.config/borders/
-ln -s ~/Git/$(whoami)/dotfiles/sketchybar/sketchybarrc ~/.config/sketchybar/
+ln -s ~/Git/$(whoami)/dotfiles/fonts ~/Library/Fonts
+ln -s ~/Git/$(whoami)/dotfiles/yabai ~/.config/yabai
+ln -s ~/Git/$(whoami)/dotfiles/skhd ~/.config/skhd
 
-echo "ITERM2 => Set the new fonts, and the colorscheme from dotfiles repo"
 echo "OTHER SOFT TO DL => ITERM2, SPARK, AUTHY, BITWARDEN, ALFRED4, CLEAN MY MAC X, RECTANGLE"
 echo "ASDF => run asdf install on your HOME folder"
-echo "NVIM run :healthcheck"
+
+csrutil status
+echo "(optional) Disable SIP for advanced yabai features."
+echo "(optional) Add sudoer manually:\n '$(whoami) ALL = (root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | awk "{print \$1;}") $(which yabai) --load-sa' to '/private/etc/sudoers.d/yabai'"
+echo "Installation complete...\n"
