d88b816ebd553760c8253c6a86299fcca14fc477
vmarlier <valentin.marlier@pm.me>
2024-07-09 12:36:41 +0200

    feat(): set lsp loglevel to WARN

--- nvim/lua/plugin_config/autocompletion.lua
+++ nvim/lua/plugin_config/autocompletion.lua
@@ -35,6 +35,10 @@ for _, lsp in ipairs(lsp_servers) do
   }
 end
 
+-- Levels by name: "TRACE", "DEBUG", "INFO", "WARN", "ERROR", "OFF"
+-- Let's keep it WARN to see if it allow a relief on the log file
+vim.lsp.set_log_level("WARN")
+
 -- luasnip setup
 local luasnip = require('luasnip') -- luasnip
 
