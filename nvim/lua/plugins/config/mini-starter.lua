ffbee081e5b0809cad4d21d3b930310f6807f82d
vmarlier <valentin.marlier@pm.me>
2024-08-05 15:51:02 +0200

    feat(): do not toggle file explorer on project select

--- nvim/lua/plugins/config/mini-starter.lua
+++ nvim/lua/plugins/config/mini-starter.lua
@@ -14,7 +14,7 @@ local function generate_repositories_items(base_path)
       local repo_name = trimmed_dir:match("([^/]+)$")
       if repo_name then
         local item_name = string.format("%s", repo_name)
-        local item_action = string.format(":lua vim.cmd('cd %s'); MiniStarter.refresh(); vim.cmd('NvimTreeToggle')",
+        local item_action = string.format(":lua vim.cmd('cd %s'); MiniStarter.refresh();",
           trimmed_dir)
         table.insert(items, { name = item_name, action = item_action, section = 'Pleo' })
       end
@@ -29,9 +29,9 @@ local pleo_repo = generate_repositories_items("~/Git/pleo")
 starter.setup({
   evaluate_single = true,
   items = {
-    { name = "Dotfiles", action = ":lua vim.cmd('cd ~/Git/valentin.marlier/dotfiles'); MiniStarter.refresh(); vim.cmd('NvimTreeToggle')", section = 'Perso' },
+    { name = "Dotfiles", action = ":lua vim.cmd('cd ~/Git/valentin.marlier/dotfiles'); MiniStarter.refresh();", section = 'Perso' },
     pleo_repo,
-    starter.sections.recent_files(5, true),
+    starter.sections.recent_files(10, true),
     starter.sections.builtin_actions(),
   },
   content_hooks = {
