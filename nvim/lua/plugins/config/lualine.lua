d433e2528ca5777a7eaadafcb4659664f4f48604
vmarlier <valentin.marlier@pm.me>
2024-10-13 22:38:51 +0200

    feat: enforce catppuccin theme in lualine

--- nvim/lua/plugins/config/lualine.lua
+++ nvim/lua/plugins/config/lualine.lua
@@ -161,7 +161,7 @@ end
 require('lualine').setup {
   options = {
     icons_enabled = true,
-    theme = 'auto',
+    theme = 'catppuccin',
     component_separators = '',
     section_separators = '',
     disabled_filetypes = {
