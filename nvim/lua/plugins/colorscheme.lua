00a4d6347cef7642708b5f22508615a6c3641de8
vmarlier <valentin.marlier@pm.me>
2025-02-12 12:30:55 +0800

    fix: colorschem conf

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -7,50 +7,47 @@ return {
     'catppuccin/nvim',
     name = 'catppuccin',
     priority = 1000,
-    opts = function()
-      local config = {
-        flavour = "frappe",
-        styles = {
-          comments = { "italic" }, -- Change the style of comments
+    opts = {
+      flavour = "frappe",
+      styles = {
+        comments = { "italic" }, -- Change the style of comments
+      },
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = true,
+        mason = true,
+        markdown = true,
+        which_key = true,
+        mini = {
+          enabled = true,
+          indentscope_color = "",
         },
-        integrations = {
-          cmp = true,
-          gitsigns = true,
-          nvimtree = true,
-          treesitter = true,
-          notify = true,
-          mason = true,
-          markdown = true,
-          which_key = true,
-          mini = {
-            enabled = true,
-            indentscope_color = "",
+        native_lsp = {
+          enabled = true,
+          virtual_text = {
+            errors = { "italic" },
+            hints = { "italic" },
+            warnings = { "italic" },
+            information = { "italic" },
           },
-          native_lsp = {
-            enabled = true,
-            virtual_text = {
-              errors = { "italic" },
-              hints = { "italic" },
-              warnings = { "italic" },
-              information = { "italic" },
-            },
-            underlines = {
-              errors = { "underline" },
-              hints = { "underline" },
-              warnings = { "underline" },
-              information = { "underline" },
-            },
-            inlay_hints = {
-              background = true,
-            },
+          underlines = {
+            errors = { "underline" },
+            hints = { "underline" },
+            warnings = { "underline" },
+            information = { "underline" },
           },
-        }
+          inlay_hints = {
+            background = true,
+          },
+        },
       }
-      return config
-    end,
-    config = function(_, config)
+    },
+    config = function(_, opts)
       local colorscheme = require("catppuccin")
-      colorscheme.setup(config)
+      colorscheme.setup(opts)
 
       vim.o.termguicolors = true
       vim.o.background = "dark"
