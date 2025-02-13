3d2a36f494eceafe20f25f00a42b27ea50c8cd70
vmarlier <valentin.marlier@pm.me>
2025-02-13 18:28:51 +0800

    fix: remove useless mapping

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -5,16 +5,16 @@ local wk = require("which-key")
 
 -- Keybindings for Normal Mode with "<space>" for prefix
 wk.add({
-  { "<space>M",  ":Mason<cr>",                                                 desc = "Open Mason package manager" },
-  { "<space>P",  ":Lazy<cr>",                                                  desc = "Open Lazy plugin manager" },
-  { "<space>e",  ":NvimTreeToggle<cr> <bar> :lua require('edgy').close()<cr>", desc = "File Tree" },
-  { "<space>h",  "<C-W>h",                                                     desc = "Go To Left Window" },
-  { "<space>j",  "<C-W>j",                                                     desc = "Go To Window Below" },
-  { "<space>k",  "<C-W>k",                                                     desc = "Go To Upper Window" },
-  { "<space>l",  "<C-W>l",                                                     desc = "Go To Right Window" },
-  { "<space>p",  require('toolbox').show_picker,                               desc = "Show toolbox picker" },
+  { "<space>M",  ":Mason<cr>",                    desc = "Open Mason package manager" },
+  { "<space>P",  ":Lazy<cr>",                     desc = "Open Lazy plugin manager" },
+  { "<space>e",  ":NvimTreeToggle<cr>",           desc = "File Tree" },
+  { "<space>h",  "<C-W>h",                        desc = "Go To Left Window" },
+  { "<space>j",  "<C-W>j",                        desc = "Go To Window Below" },
+  { "<space>k",  "<C-W>k",                        desc = "Go To Upper Window" },
+  { "<space>l",  "<C-W>l",                        desc = "Go To Right Window" },
+  { "<space>p",  require('toolbox').show_picker,  desc = "Show toolbox picker" },
   { "<space>t",  group = "Terminal" },
-  { "<space>tb", ":ToggleTerm direction=tab<cr>",                              desc = "Open terminal tab" },
+  { "<space>tb", ":ToggleTerm direction=tab<cr>", desc = "Open terminal tab" },
 })
 
 -- Keybindings for Normal Mode with "<TAB>" for prefix
