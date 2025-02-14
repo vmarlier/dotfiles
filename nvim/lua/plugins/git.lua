660f3b849e5609ea203c9f06ea8206d2a7e4c171
vmarlier <valentin.marlier@pm.me>
2025-02-14 11:10:07 +0800

    feat(): add vim-fugitive

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -3,6 +3,9 @@
 -----------------------------------
 -- Git Integration
 return {
+  { -- Git integration
+    "tpope/vim-fugitive",
+  },
   { -- Enhance git support
     "lewis6991/gitsigns.nvim",
     opts = {},
