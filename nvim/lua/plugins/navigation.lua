e885b287ba623004149f8ef217ce8e5cc499f8b2
vmarlier <valentin.marlier@pm.me>
2025-02-15 18:39:28 +0800

    fix: get rid of unused plugins

--- nvim/lua/plugins/navigation.lua
+++ nvim/lua/plugins/navigation.lua
@@ -114,41 +114,6 @@ return {
       })
     end
   },
-  { -- File explorer but just for Git integration
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    },
-    opts = {
-      close_if_last_window = false,
-      enable_git_status = true,
-      enable_diagnostics = false,
-      mdefault_component_configs = {
-        file_size = {
-          enabled = false,
-        },
-        type = {
-          enabled = false,
-        },
-        last_modified = {
-          enabled = false,
-        },
-        created = {
-          enabled = false,
-        },
-        symlink_target = {
-          enabled = false,
-        },
-      },
-      window = {
-        position = "left",
-        width = 30,
-      }
-    }
-  },
   { -- Symbols explorer, used in edgy sidebar
     'simrat39/symbols-outline.nvim',
     opts = {
