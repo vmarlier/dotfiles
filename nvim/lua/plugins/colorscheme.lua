5771bcb2cd6adcaa4871a024bb9289dff873ee63
vmarlier <valentin.marlier@pm.me>
2025-02-22 20:10:29 +0800

    feat: use shadow.nvim main

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -3,23 +3,23 @@
 -----------------------------------
 -- Colorscheme
 return {
-  --{
-  --  "rjshkhr/shadow.nvim",
-  --  priority = 1000,
-  --  config = function()
-  --    vim.opt.termguicolors = true
-  --    vim.opt.background = "dark"
-  --    vim.cmd.colorscheme("shadow")
-  --  end,
-  --},
   {
-    "vmarlier/shadow.nvim",
+    "rjshkhr/shadow.nvim",
     priority = 1000,
-    branch = "custom-main",
     config = function()
       vim.opt.termguicolors = true
       vim.opt.background = "dark"
       vim.cmd.colorscheme("shadow")
     end,
   },
+  -- {
+  --   "vmarlier/shadow.nvim",
+  --   priority = 1000,
+  --   branch = "custom-main",
+  --   config = function()
+  --     vim.opt.termguicolors = true
+  --     vim.opt.background = "dark"
+  --     vim.cmd.colorscheme("shadow")
+  --   end,
+  -- },
 }
