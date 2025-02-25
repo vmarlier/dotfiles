4e0ef537ae4e7794ff673506c7fc3e70f8774c01
vmarlier <valentin.marlier@pm.me>
2025-02-25 20:20:33 +0800

    feat: replace git in nvim with dedicated iterm tab for lazygit

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -3,14 +3,11 @@
 -----------------------------------
 -- Git Integration
 return {
-  { -- Git integration
-    "tpope/vim-fugitive",
-  },
   { -- Enhance git support
     "lewis6991/gitsigns.nvim",
     opts = {},
   },
-  { -- Show git blame inline
+  { -- Show git blame - But used to open file/selection in browser
     "f-person/git-blame.nvim",
     config = function()
       local gitblame = require("gitblame")
