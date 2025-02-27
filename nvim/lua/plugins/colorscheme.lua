ca4b6ef9ff424c5305137521f786ac8c59c70c85
vmarlier <valentin.marlier@pm.me>
2025-02-27 20:43:13 +0800

    fix: remove useless commented plugin

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -12,14 +12,4 @@ return {
       vim.cmd.colorscheme("shadow")
     end,
   },
-  -- {
-  --   "vmarlier/shadow.nvim",
-  --   priority = 1000,
-  --   branch = "custom-main",
-  --   config = function()
-  --     vim.opt.termguicolors = true
-  --     vim.opt.background = "dark"
-  --     vim.cmd.colorscheme("shadow")
-  --   end,
-  -- },
 }
