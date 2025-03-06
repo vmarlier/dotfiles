0b830a17f0ae782505d9a7bd7f81232b3eb6bc90
vmarlier <valentin.marlier@pm.me>
2025-03-06 12:58:43 +0800

    feat: replace bufferline with dedicated tabline

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -7,52 +7,26 @@ return {
     "nvim-tree/nvim-web-devicons",
     opts = {},
   },
-  { -- Configurable bufferline
-    "akinsho/bufferline.nvim",
+  {
+    "seblyng/nvim-tabline",
     opts = {
-      options = {
-        numbers = "ordinal",
-        indicator = { style = "icon", icon = "▎" },
-        modified_icon = '●',
-        left_trunc_marker = '',
-        right_trunc_marker = '',
-        name_formatter = function(buf) -- buf contains a "name", "path" and "bufnr"
-          if buf.name:match('%.md') then
-            return vim.fn.fnamemodify(buf.name, ':t:r')
-          end
-        end,
-        max_name_length = 18,
-        max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
-        tab_size = 18,
-        diagnostics = false,
-        --diagnostics = "nvim_lsp",
-        diagnostics_update_in_insert = false,
-        diagnostics_indicator = function(count, level, diagnostics_dict, context)
-          local s = " "
-          for e, n in pairs(diagnostics_dict) do
-            local sym = e == "error" and " "
-                or (e == "warning" and " " or "")
-            s = s .. n .. sym
-          end
-          return s
-        end,
-        offsets = {
-          {
-            filetype = "NvimTree",
-            text = "File Explorer"
-          }
-        },
-        show_buffer_icons = true, -- disable filetype icons for buffers
-        show_buffer_close_icons = false,
-        show_close_icon = false,
-        show_tab_indicators = true,
-        persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
-        separator_style = "thick",
-        enforce_regular_tabs = false,
-        always_show_bufferline = true,
-        sort_by = 'id'
+      no_name = '[No Name]', -- Name for buffers with no name
+      modified_icon = '', -- Icon for showing modified buffer
+      close_icon = '', -- Icon for closing tab with mouse
+      separator = '▌', -- Separator icon on the left side
+      padding = 3, -- Prefix and suffix space
+      color_all_icons = false, -- Color devicons in active and inactive tabs
+      right_separator = false, -- Show right separator on the last tab
+      show_index = true, -- Shows the index of tab before filename
+      show_icon = true, -- Shows the devicon
+      show_window_count = {
+        enable = true, -- Shows the number of windows in tab after filename
+        show_if_alone = false, -- do not show count if unique win in a tab
+        count_unique_buf = false, -- count only win showing different buffers
+        count_others = true, -- display [+x] where x is the number of other windows
+        buftype_blacklist = { 'nofile' }, -- do not count if buftype among theses
       },
-    },
+    }
   },
   { -- Enhance folding capabilities
     "kevinhwang91/nvim-ufo",
