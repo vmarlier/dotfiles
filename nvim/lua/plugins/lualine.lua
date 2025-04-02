34f9afd1467271d8b6df7ef5b18f9a59645406e0
vmarlier <valentin.marlier@pm.me>
2025-04-02 15:36:06 +0800

    fix: replace deprecated vim.lsp.get_active_client

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -129,7 +129,9 @@ return {
       local function lsp()
         local msg = ' LSP: No Active Lsp'
         local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-        local clients = vim.lsp.get_active_clients()
+        local clients = vim.lsp.get_clients({
+          bufnr = 0
+        })
         if next(clients) == nil then
           return msg
         end
